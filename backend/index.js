0b791726ffd1fd6b60697a607d1a5722b0cee7f5
Anish <engineeranish10000@gmail.com>
2025-04-08 17:32:41 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./models/config');
+const userRoutes = require('./routes/user');
 
-require('./models/config');
-require('./models/Food')
+dotenv.config();
+connectDB();
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
-const PORT = process.env.PORT || 5000
 
-app.use('/api/products', require('./routes/products'));
+app.use('/api/users', userRoutes);
 
-app.listen(PORT,()=>{
-    console.log("Connected to http://localhost:5000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
